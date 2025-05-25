26bc8b28b6cd412dbd4bf6096a52a68678320978
MarigoldJ <yusang0130@gmail.com>
2025-05-25 21:46:02 +0900

    feat: next.config.js 파일 삭제 및 next.config.ts로 설정 통합

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,19 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "ddragon.leagueoflegends.com",
+        pathname: "**",
+      },
+    ],
+  },
+  env: {
+    // Define a fallback as empty string when env var is not available
+    NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL || "",
+  },
 };
 
 export default nextConfig;
